b2917800e46239c1140f922f3e59005db8859c1d
Matthew <matthewtoro17@gmail.com>
2022-03-19 00:23:51 +0800

    Create function that plays a single round

--- script.js
+++ script.js
@@ -3,15 +3,53 @@ function computerPlay() {
 
     switch (randNum) {
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1:
-            return "Paper";
+            return "paper";
             break;
         case 2:
-            return "Scissors";
+            return "scissors";
             break;
         default:
             return "Something has gone horribly wrong.";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                return "It's a draw!";
+            } else if (computerSelection === "paper") {
+                return "You lose! Paper beats rock.";
+            } else {
+                return "You win! Rock beats scissors.";
+            }
+            break;
+        case "paper":
+            if (computerSelection === "rock") {
+                return "You win! Paper beats rock.";
+            } else if (computerSelection === "paper") {
+                return "It's a draw!";
+            } else {
+                return "You lose! Scissors beats paper.";
+            }
+            break;
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You lose! Rock beats scissors.";
+            } else if (computerSelection === "paper") {
+                return "You win! Scissors beats paper.";
+            } else {
+                return "It's a draw!";
+            }
+            break;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
